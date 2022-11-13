3fceb803d528e997e7b3cd7a576b27dbef8df335
accup <32096121+accup@users.noreply.github.com>
2022-11-13 22:45:09 +0900

    refactor(vanilla-extract-color): implement translucentColorFromHex

--- packages/vanilla-extract-color/lib/color.ts
+++ packages/vanilla-extract-color/lib/color.ts
@@ -4,8 +4,14 @@ export interface OpaqueColor {
   b: number;
 }
 
-const RE_HEX = /^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/;
+export type Opacity = number;
 
+export interface TranslucentColor {
+  opaqueColor: OpaqueColor;
+  opacity: Opacity;
+}
+
+const RE_HEX = /^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/;
 export function opaqueColorFromHex(hex: string): OpaqueColor {
   const match = hex.match(RE_HEX);
   if (match == null) {
@@ -16,5 +22,23 @@ export function opaqueColorFromHex(hex: string): OpaqueColor {
     r: parseInt(match[1], 16),
     g: parseInt(match[2], 16),
     b: parseInt(match[3], 16),
+  };
+}
+
+const RE_HEX_WITH_OPACITY =
+  /^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/;
+export function translucentColorFromHex(hex: string): TranslucentColor {
+  const match = hex.match(RE_HEX_WITH_OPACITY);
+  if (match == null) {
+    throw new Error(`Invalid hex color representation`);
+  }
+
+  return {
+    opaqueColor: {
+      r: parseInt(match[1], 16),
+      g: parseInt(match[2], 16),
+      b: parseInt(match[3], 16),
+    },
+    opacity: parseInt(match[4], 16) / 255.0,
   };
 }
--- packages/vanilla-extract-color/test/color.test.ts
+++ packages/vanilla-extract-color/test/color.test.ts
@@ -1,4 +1,4 @@
-import { opaqueColorFromHex } from "../lib/color.js";
+import { opaqueColorFromHex, translucentColorFromHex } from "../lib/color.js";
 
 describe("opaqueColorFromHex", () => {
   it.each([
@@ -17,5 +17,37 @@ describe("opaqueColorFromHex", () => {
     { hex: "#1428576" },
   ])("with hex: $hex throws error", ({ hex }) => {
     expect(() => opaqueColorFromHex(hex)).toThrow();
+  });
+});
+
+describe("translucentColorFromHex", () => {
+  it.each([
+    {
+      hex: "#00000000",
+      expected: { opaqueColor: { r: 0, g: 0, b: 0 }, opacity: 0.0 },
+    },
+    {
+      hex: "#12345633",
+      expected: { opaqueColor: { r: 18, g: 52, b: 86 }, opacity: 0.2 },
+    },
+    {
+      hex: "#acfacfcc",
+      expected: { opaqueColor: { r: 172, g: 250, b: 207 }, opacity: 0.8 },
+    },
+    {
+      hex: "#f7b8d9ff",
+      expected: { opaqueColor: { r: 247, g: 184, b: 217 }, opacity: 1.0 },
+    },
+  ])("with hex: $hex returns $expected", ({ hex, expected }) => {
+    expect(translucentColorFromHex(hex)).toEqual(expected);
+  });
+
+  it.each([
+    { hex: "" },
+    { hex: "12345678" },
+    { hex: "#acgacfac" },
+    { hex: "#142857614" },
+  ])("with hex: $hex throws error", ({ hex }) => {
+    expect(() => translucentColorFromHex(hex)).toThrow();
   });
 });
